ff6430397312fa6f9e4de5821bba235a1f848ad4
Nguyễn Đức Tuấn Anh <nhongbay2112bt@gmail.com>
2018-12-13 17:33:32 +0700

    Revert "Add Identity"

--- Startup.cs
+++ Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using AspNetCore.RouteAnalyzer;
 
 namespace dangVienManagement
 {
@@ -16,43 +15,19 @@ namespace dangVienManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-            services.AddRouteAnalyzer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
-        IApplicationLifetime applicationLifetime, IRouteAnalyzer routeAnalyzer)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseHsts();
-            }
 
-            applicationLifetime.ApplicationStarted.Register(() =>
+            app.Run(async (context) =>
             {
-                var infos = routeAnalyzer.GetAllRouteInformations();
-                Console.WriteLine("======== ALL ROUTE INFORMATION ========");
-                foreach (var info in infos)
-                {
-                    Console.WriteLine(info.ToString());
-                }
-                Console.WriteLine("");
-                Console.WriteLine("");
-            });
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseAuthentication();
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
